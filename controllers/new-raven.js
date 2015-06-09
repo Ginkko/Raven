dafee877cf08b47f1c8f75cc6c47fb2171e4fe48
Alec Arme & Ben Cornelis <student@epicodus.com>
2015-06-09 14:12:20 -0700

    Add hashtag splitting support. Bugfix overwriting new ravens and improper indexing.

--- controllers/new-raven.js
+++ controllers/new-raven.js
@@ -1,22 +1,28 @@
 App.NewRavenController = Ember.Controller.extend({
   messageError: false,
   houseTagsHolder: "",
+
   raven: {
-    id: ravens.length.toString(),
     userName: "",
     text: "",
-    houseTags: [],
-    parentRaven: undefined
   },
 
   actions: {
     saveRaven: function() {
+      var newRaven = {id: ravens.length.toString(), userName: this.raven.userName, text: this.raven.text, houseTags: [], parentRaven: undefined};
       if (this.raven.text.length > 141) {
         this.set('messageError', true);
       } else {
-        this.raven.houseTags.addObject(this.houseTagsHolder);
-        ravens.addObject(this.raven);
+        var houseTags = this.houseTagsHolder.split(', ');
+        for (var i = 0; i < houseTags.length; i++) {
+          newRaven.houseTags.push(houseTags[i]);
+        }
+        ravens.addObject(newRaven);
+        this.set('raven.userName', '');
+        this.set('raven.text', '');
+        this.set('houseTagsHolder', '');
         this.transitionToRoute('ravens');
+
       }
     }
   }
